8c08ee104e44abc04257bff41841c7ee2f10966b
htrobles <hjt.robles@gmail.com>
2024-11-20 21:02:08 -0500

    add contacts

--- src/components/home/HomeContact/HomeContact.tsx
+++ src/components/home/HomeContact/HomeContact.tsx
@@ -12,6 +12,7 @@ import {
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { Mail, MapPin, Phone } from 'lucide-react';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 
@@ -45,7 +46,7 @@ export default function HomeContact() {
   return (
     <div className='container mx-auto py-12 space-y-12'>
       <h2>Connect with Us</h2>
-      <div className='grid grid-cols-1 md:grid-cols-2'>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-12'>
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
             <FormField
@@ -96,6 +97,31 @@ export default function HomeContact() {
             </Button>
           </form>
         </Form>
+        <div className='space-y-4'>
+          <iframe
+            src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2919.4802778963717!2d-81.21649682270161!3d42.96815559646576!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882e9311f4d69b83%3A0x90c3e2576ae44111!2sGospelTrend%20London!5e0!3m2!1sen!2sca!4v1732151420060!5m2!1sen!2sca'
+            // width={600}
+            // height={450}
+            allowFullScreen
+            loading='lazy'
+            referrerPolicy='no-referrer-when-downgrade'
+            className='w-full aspect-[7/4] rounded-lg'
+          />
+          <div className='space-y-4'>
+            <div className='flex gap-x-4'>
+              <MapPin />
+              <div>363 Westlake St, London, ON N5Z 3A5</div>
+            </div>
+            <div className='flex gap-x-4'>
+              <Phone />
+              <div>(416) 797-3685</div>
+            </div>
+            <div className='flex gap-x-4'>
+              <Mail />
+              <div>gospeltrendlondon@gmail.com</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
