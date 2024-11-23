5cdae1d3b22f25578a9735f1d72896d1fe56a23d
htrobles <hjt.robles@gmail.com>
2024-11-22 19:55:27 -0500

    Revert "include email in contact form"
    This reverts commit e8b71ee77f4cb3206ff6ed1ec57f93b174295e1c.

--- src/components/home/Hero/HeroImages.tsx
+++ src/components/home/Hero/HeroImages.tsx
@@ -2,60 +2,60 @@ import Image from 'next/image';
 
 export default function HeroImages() {
   return (
-    <div className="flex-1 flex justify-end items-center gap-2 lg:order-2 w-full">
-      <div className="flex gap-2 flex-col w-full">
+    <div className='flex-1 flex justify-end items-center gap-2 lg:order-2 w-full'>
+      <div className='flex gap-2 flex-col w-full'>
         <Image
-          className="aspect-[4/3] lg:aspect-[3/4] object-cover rounded-lg object-center w-full"
-          src="/hero-images/hero-image-1.jpeg"
-          width={2048}
-          height={1536}
-          alt="Hero Image"
-        />
-        <Image
-          className="aspect-[4/3] lg:aspect-[3/4] object-cover rounded-lg object-center w-full"
-          src="/hero-images/hero-image-2.jpeg"
-          width={2048}
-          height={1536}
-          alt="Hero Image"
-        />
-      </div>
-      <div className="flex gap-2 flex-col w-full">
-        <Image
-          className="aspect-[4/3] lg:aspect-[3/4] object-cover rounded-lg object-center w-full"
-          src="/hero-images/hero-image-3.jpeg"
-          width={1536}
-          height={1152}
-          alt="Hero Image"
-        />
-        <Image
-          className="aspect-[4/3] lg:aspect-[3/4] object-cover rounded-lg object-center w-full"
-          src="/hero-images/hero-image-4.jpg"
-          width={2000}
-          height={3000}
-          alt="Hero Image"
-        />
-        <Image
-          className="aspect-[4/3] lg:aspect-[3/4] object-cover rounded-lg object-center w-full"
-          src="/hero-images/hero-image-5.jpeg"
+          className='aspect-[4/3] lg:aspect-[3/4] object-cover rounded-lg object-center w-full'
+          src='/hero-images/hero-image-1.png'
           width={168}
           height={214}
-          alt="Hero Image"
+          alt='Hero Image'
+        />
+        <Image
+          className='aspect-[4/3] lg:aspect-[3/4] object-cover rounded-lg object-center w-full'
+          src='/hero-images/hero-image-2.png'
+          width={168}
+          height={214}
+          alt='Hero Image'
         />
       </div>
-      <div className="flex gap-2 flex-col w-full">
+      <div className='flex gap-2 flex-col w-full'>
         <Image
-          className="aspect-[4/3] lg:aspect-[3/4] object-cover rounded-lg object-center w-full"
-          src="/hero-images/hero-image-6.jpeg"
-          width={2048}
-          height={946}
-          alt="Hero Image"
+          className='aspect-[4/3] lg:aspect-[3/4] object-cover rounded-lg object-center w-full'
+          src='/hero-images/hero-image-3.png'
+          width={168}
+          height={214}
+          alt='Hero Image'
         />
         <Image
-          className="aspect-[4/3] lg:aspect-[3/4] object-cover rounded-lg object-center w-full"
-          src="/hero-images/hero-image-7.jpeg"
-          width={2048}
-          height={1536}
-          alt="Hero Image"
+          className='aspect-[4/3] lg:aspect-[3/4] object-cover rounded-lg object-center w-full'
+          src='/hero-images/hero-image-4.png'
+          width={168}
+          height={214}
+          alt='Hero Image'
+        />
+        <Image
+          className='aspect-[4/3] lg:aspect-[3/4] object-cover rounded-lg object-center w-full'
+          src='/hero-images/hero-image-5.png'
+          width={168}
+          height={214}
+          alt='Hero Image'
+        />
+      </div>
+      <div className='flex gap-2 flex-col w-full'>
+        <Image
+          className='aspect-[4/3] lg:aspect-[3/4] object-cover rounded-lg object-center w-full'
+          src='/hero-images/hero-image-6.png'
+          width={168}
+          height={214}
+          alt='Hero Image'
+        />
+        <Image
+          className='aspect-[4/3] lg:aspect-[3/4] object-cover rounded-lg object-center w-full'
+          src='/hero-images/hero-image-7.png'
+          width={168}
+          height={214}
+          alt='Hero Image'
         />
       </div>
     </div>
--- src/components/home/HomeConnect/HomeConnect.tsx
+++ src/components/home/HomeConnect/HomeConnect.tsx
@@ -11,16 +11,25 @@ import {
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
-import sendConnectForm from '@/lib/actions/sendConnectForm';
-import { connectFormSchema } from '@/models/formSchemas/connectFormSchema';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Mail, MapPin, Phone } from 'lucide-react';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 
+const formSchema = z.object({
+  email: z
+    .string()
+    .email({ message: 'Please enter a valid email address' })
+    .min(5),
+  name: z.string().min(2, { message: 'Please enter your name' }),
+  message: z
+    .string()
+    .min(2, { message: 'Please let us know how we can help you' }),
+});
+
 export default function HomeConnect() {
-  const form = useForm<z.infer<typeof connectFormSchema>>({
-    resolver: zodResolver(connectFormSchema),
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
     defaultValues: {
       email: '',
       name: '',
@@ -28,27 +37,26 @@ export default function HomeConnect() {
     },
   });
 
-  function onSubmit(values: z.infer<typeof connectFormSchema>) {
+  function onSubmit(values: z.infer<typeof formSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     console.log(values);
-    sendConnectForm(values);
   }
 
   return (
-    <section id="connect" className="container mx-auto py-12 space-y-12">
+    <section id='connect' className='container mx-auto py-12 space-y-12'>
       <h2>Connect with Us</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-12'>
         <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
             <FormField
               control={form.control}
-              name="name"
+              name='name'
               render={({ field }) => (
                 <FormItem>
                   <FormLabel>Name</FormLabel>
                   <FormControl>
-                    <Input placeholder="Enter your name" {...field} />
+                    <Input placeholder='Enter your name' {...field} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -56,12 +64,12 @@ export default function HomeConnect() {
             />
             <FormField
               control={form.control}
-              name="email"
+              name='email'
               render={({ field }) => (
                 <FormItem>
                   <FormLabel>Email Address</FormLabel>
                   <FormControl>
-                    <Input placeholder="Enter your email address" {...field} />
+                    <Input placeholder='Enter your email address' {...field} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -69,13 +77,13 @@ export default function HomeConnect() {
             />
             <FormField
               control={form.control}
-              name="message"
+              name='message'
               render={({ field }) => (
                 <FormItem>
                   <FormLabel>Message</FormLabel>
                   <FormControl>
                     <Textarea
-                      placeholder="Enter your message"
+                      placeholder='Enter your message'
                       {...field}
                       rows={8}
                     />
@@ -84,31 +92,31 @@ export default function HomeConnect() {
                 </FormItem>
               )}
             />
-            <Button type="submit" variant="black" className="w-full">
+            <Button type='submit' variant='black' className='w-full'>
               Submit
             </Button>
           </form>
         </Form>
-        <div className="space-y-4">
+        <div className='space-y-4'>
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2919.4802778963717!2d-81.21649682270161!3d42.96815559646576!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882e9311f4d69b83%3A0x90c3e2576ae44111!2sGospelTrend%20London!5e0!3m2!1sen!2sca!4v1732151420060!5m2!1sen!2sca"
+            src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2919.4802778963717!2d-81.21649682270161!3d42.96815559646576!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882e9311f4d69b83%3A0x90c3e2576ae44111!2sGospelTrend%20London!5e0!3m2!1sen!2sca!4v1732151420060!5m2!1sen!2sca'
             // width={600}
             // height={450}
             allowFullScreen
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-            className="w-full aspect-[7/4] rounded-lg"
+            loading='lazy'
+            referrerPolicy='no-referrer-when-downgrade'
+            className='w-full aspect-[7/4] rounded-lg'
           />
-          <div className="space-y-4">
-            <div className="flex gap-x-4">
+          <div className='space-y-4'>
+            <div className='flex gap-x-4'>
               <MapPin />
               <div>363 Westlake St, London, ON N5Z 3A5</div>
             </div>
-            <div className="flex gap-x-4">
+            <div className='flex gap-x-4'>
               <Phone />
               <div>(416) 797-3685</div>
             </div>
-            <div className="flex gap-x-4">
+            <div className='flex gap-x-4'>
               <Mail />
               <div>gospeltrendlondon@gmail.com</div>
             </div>
