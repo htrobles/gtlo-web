c96438ed6b12b8708b02ecba704ae606b754233d
htrobles <hjt.robles@gmail.com>
2024-11-22 22:22:55 -0500

    include contact form submit feedback

--- src/components/home/HomeConnect/HomeConnect.tsx
+++ src/components/home/HomeConnect/HomeConnect.tsx
@@ -11,6 +11,8 @@ import {
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
+import { ToastAction } from '@/components/ui/toast';
+import { useToast } from '@/hooks/use-toast';
 import sendConnectForm from '@/lib/actions/sendConnectForm';
 import { connectFormSchema } from '@/models/formSchemas/connectFormSchema';
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -19,6 +21,8 @@ import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 
 export default function HomeConnect() {
+  const { toast } = useToast();
+
   const form = useForm<z.infer<typeof connectFormSchema>>({
     resolver: zodResolver(connectFormSchema),
     defaultValues: {
@@ -28,27 +32,43 @@ export default function HomeConnect() {
     },
   });
 
-  function onSubmit(values: z.infer<typeof connectFormSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
-    sendConnectForm(values);
-  }
+  const onSubmit = async (values: z.infer<typeof connectFormSchema>) => {
+    const res = await sendConnectForm(values);
+    if (res.error) {
+      console.log(res.error);
+      toast({
+        variant: 'destructive',
+        title: 'Sorry. Something went wrong.',
+        description: 'There was a problem with your request.',
+        action: <ToastAction altText='Try again'>Try again</ToastAction>,
+      });
+    } else {
+      toast({
+        variant: 'default',
+        title: 'Thank you for reaching out to us!',
+        description:
+          'Our Connect Team will contact you as soon as they read your message.',
+        action: <ToastAction altText='Confirm'>Confirm</ToastAction>,
+      });
+
+      form.reset();
+    }
+  };
 
   return (
-    <section id="connect" className="container mx-auto py-12 space-y-12">
+    <section id='connect' className='container mx-auto py-12 space-y-12'>
       <h2>Connect with Us</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-12'>
         <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
             <FormField
               control={form.control}
-              name="name"
+              name='name'
               render={({ field }) => (
                 <FormItem>
                   <FormLabel>Name</FormLabel>
                   <FormControl>
-                    <Input placeholder="Enter your name" {...field} />
+                    <Input placeholder='Enter your name' {...field} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -56,12 +76,12 @@ export default function HomeConnect() {
             />
             <FormField
               control={form.control}
-              name="email"
+              name='email'
               render={({ field }) => (
                 <FormItem>
                   <FormLabel>Email Address</FormLabel>
                   <FormControl>
-                    <Input placeholder="Enter your email address" {...field} />
+                    <Input placeholder='Enter your email address' {...field} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -69,13 +89,13 @@ export default function HomeConnect() {
             />
             <FormField
               control={form.control}
-              name="message"
+              name='message'
               render={({ field }) => (
                 <FormItem>
                   <FormLabel>Message</FormLabel>
                   <FormControl>
                     <Textarea
-                      placeholder="Enter your message"
+                      placeholder='Enter your message'
                       {...field}
                       rows={8}
                     />
@@ -84,31 +104,31 @@ export default function HomeConnect() {
                 </FormItem>
               )}
             />
-            <Button type="submit" variant="black" className="w-full">
+            <Button type='submit' variant='black' className='w-full'>
               Submit
             </Button>
           </form>
         </Form>
-        <div className="space-y-4">
+        <div className='space-y-4'>
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2919.4802778963717!2d-81.21649682270161!3d42.96815559646576!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882e9311f4d69b83%3A0x90c3e2576ae44111!2sGospelTrend%20London!5e0!3m2!1sen!2sca!4v1732151420060!5m2!1sen!2sca"
+            src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2919.4802778963717!2d-81.21649682270161!3d42.96815559646576!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882e9311f4d69b83%3A0x90c3e2576ae44111!2sGospelTrend%20London!5e0!3m2!1sen!2sca!4v1732151420060!5m2!1sen!2sca'
             // width={600}
             // height={450}
             allowFullScreen
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-            className="w-full aspect-[7/4] rounded-lg"
+            loading='lazy'
+            referrerPolicy='no-referrer-when-downgrade'
+            className='w-full aspect-[7/4] rounded-lg'
           />
-          <div className="space-y-4">
-            <div className="flex gap-x-4">
+          <div className='space-y-4'>
+            <div className='flex gap-x-4'>
               <MapPin />
               <div>363 Westlake St, London, ON N5Z 3A5</div>
             </div>
-            <div className="flex gap-x-4">
+            <div className='flex gap-x-4'>
               <Phone />
               <div>(416) 797-3685</div>
             </div>
-            <div className="flex gap-x-4">
+            <div className='flex gap-x-4'>
               <Mail />
               <div>gospeltrendlondon@gmail.com</div>
             </div>
